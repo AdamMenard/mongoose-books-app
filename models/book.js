3617006a25b8a0d06dbdd329b599fe5f55974316
AdamMenard <adammenard888@gmail.com>
2017-12-13 12:11:13 -0800

    created bookSchema and connecting mongoose

---
+++ models/book.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// CREATE SCHEMA
+var BookSchema = new Schema({
+      title: String,
+      author: String,
+      image: String,
+      releaseDate: Date,
+});
+
+var Book = mongoose.model('Book', BookSchema);
+
+
+
+
+// EXPORTS
+ module.exports = Book;
--- models/index.js
+++ models/index.js
@@ -1,2 +1,8 @@
+// REQUIRES MONGOOSE
 var mongoose = require("mongoose");
+// CONNECTS TO A LOCAL BOOK-APP DATABASE URI
 mongoose.connect("mongodb://localhost/book-app");
+
+
+// EXPORT BOOK MODEL INTO BOOK.JS
+module.exports.Book = require("./book.js");
