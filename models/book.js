ae340125643d46354fa5aa0218f438d27b92a931
AdamMenard <adammenard888@gmail.com>
2017-12-13 12:30:03 -0800

    connected index.js with models, i think

--- models/book.js
+++ models/book.js
@@ -1,5 +1,10 @@
 var mongoose = require('mongoose');
+// CONNECTS TO A LOCAL BOOK-APP DATABASE URI
+mongoose.connect("mongodb://localhost/book-app");
+
 var Schema = mongoose.Schema;
+
+
 
 // CREATE SCHEMA
 var BookSchema = new Schema({
